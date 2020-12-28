9ad0454dd00a8e64d3f393aa82a90dc74b8f3048
Anton Petrov <webdevelop827@gmail.com>
2020-12-28 16:18:10 +0700

    fix(design): Fix Responsive Design in Restauran List Page

--- src/app/modules/feature/auth/pages/register/register.component.ts
+++ src/app/modules/feature/auth/pages/register/register.component.ts
@@ -53,7 +53,7 @@ export class RegisterComponent implements OnInit, OnDestroy {
       name: ['', [Validators.required,
       Validators.pattern('^[A-zĂÂÎȘȚăâîșțÁÉÍÓÖŐÚÜŰáéíóöőúüű ]+$'), Validators.minLength(3), Validators.maxLength(20)]],
       email: ['', [Validators.required,
-      Validators.pattern('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$')]],
+      Validators.pattern('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@+([a-zA-Z0-9-]+\.+[a-zA-Z0-9-]+)+$')]],
       password: ['', [Validators.required,
       Validators.minLength(6), Validators.maxLength(30)]],
       passwordConfirm: ['', Validators.required],
